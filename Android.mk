8e43fdb9aee8cbc1b9d880669441648dbb368f1a
Steven Kabbes <stevenkabbes@gmail.com>
2012-02-02 16:35:45 -0800

    Modify Android.mk and Application.mk
    Change the default makefiles to actually build something

--- Android.mk
+++ Android.mk
@@ -1,13 +1,37 @@
 # Set up the following environment variables
 # NODE_ROOT: location of the node root directory (for include files)
 # ANODE_ROOT: location of the anode root directory (for the libjninode binary)
+ANODE_ROOT := /Users/steven/projects/android-hack/skabbes-anode
+NODE_ROOT := $(ANODE_ROOT)/../node
+
+# =============================================
+# STEP 1 - buliding sqlite3 as a shared library
+# =============================================
 
 # Variable definitions for Android applications
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Modify this line to configure to the module name.
-LOCAL_MODULE := hello
+LOCAL_MODULE := sqlite3
+LOCAL_SRC_FILES :=  deps/sqlite3.c
+
+# The STL implementation we chose earlier
+LOCAL_SHARED_LIBRARIES := gnustl_shared
+
+# looked up build flags from the wscript file
+LOCAL_CFLAGS := -DSQLITE_ENABLE_RTREE=1 -fPIC -O3 -DNDEBUG
+
+include $(BUILD_SHARED_LIBRARY)
+
+# =============================================
+# STEP 2 - building the sqlite3 node bindings
+# =============================================
+
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION :=.cc
+
+LOCAL_MODULE := sqlite3_bindings
 
 # Add any additional defines or compiler flags.
 # Do not delete these existing flags as these are required
@@ -17,12 +41,14 @@ LOCAL_CFLAGS := \
 	-DBUILDING_NODE_EXTENSION \
 	-include sys/select.h
 
-LOCAL_CPPFLAGS :=
+# grabbed these compiler flags out of the wscript file for node-sqlite3
+LOCAL_CPPFLAGS := -g -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -DSQLITE_ENABLE_RTREE=1 -pthread -Wall
 
 # Add any additional required directories for the include path.
 # Do not delete these existing directories as these are required
 # for the included node header files.
 LOCAL_C_INCLUDES := $(NODE_ROOT)/src \
+	./deps \
 	$(NODE_ROOT)/deps/v8/include \
 	$(NODE_ROOT)/deps/uv/include
 
@@ -30,13 +56,13 @@ LOCAL_C_INCLUDES := $(NODE_ROOT)/src \
 LOCAL_LDLIBS := \
 	$(ANODE_ROOT)/libs/armeabi/libjninode.so
 
-LOCAL_CPP_EXTENSION := .cc
-
 LOCAL_SRC_FILES :=\
-	src/hello.cc
+	src/database.cc \
+	src/sqlite3.cc \
+	src/statement.cc
 
-LOCAL_STATIC_LIBRARIES := 
+# say that this node addon depends on the sqlite3 shared library
+LOCAL_SHARED_LIBRARIES := sqlite3
 
 # Do not edit this line.
 include $(ANODE_ROOT)/sdk/addon/build-node-addon.mk
-
--- Application.mk
+++ Application.mk
@@ -1,3 +1,4 @@
+APP_STL := gnustl_shared
 APP_PROJECT_PATH :=.
 APP_BUILD_SCRIPT :=$(APP_PROJECT_PATH)/Android.mk
 APP_PLATFORM     := android-9
